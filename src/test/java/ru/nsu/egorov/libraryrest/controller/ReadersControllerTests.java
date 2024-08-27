c42969d12b0520a1b723fc71ec95b8ebd948ad97
egcrcu <danil.egorov.1980@gmail.com>
2024-08-27 15:01:34 +0700

    lil tests (HttpStatus and MediaType)

---
+++ src/test/java/ru/nsu/egorov/libraryrest/controller/ReadersControllerTests.java
@@ -0,0 +1,32 @@
+package ru.nsu.egorov.libraryrest.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ReadersControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void responseTest() throws Exception {
+
+        ResultActions result = mockMvc.perform(get("/readers")
+                .accept(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+    }
+
+}
